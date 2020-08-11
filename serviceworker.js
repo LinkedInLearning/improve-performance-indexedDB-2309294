2de2327baad73acce855cb0a78868c19a818b9a2
Mike Oram <mike.oram@mayden.co.uk>
2020-08-11 19:04:10 +0100

    RECORDED 02_04 SUCCESSFULLY

--- serviceworker.js
+++ serviceworker.js
@@ -1,4 +1,23 @@
+const currentCaches = {
+    css: 'CSS',
+    imgs: 'images'
+}
+
 self.addEventListener("install", event => {
 
+    event.waitUntil(
+        Promise.all(
+            [
+                caches.open(currentCaches.css)
+                    .then(cache => {
+                        // the cache is open
+                    }),
+                caches.open(currentCaches.imgs)
+                    .then(cache => {
+
+                    })
+            ]
+        )
+    )
 
 })
