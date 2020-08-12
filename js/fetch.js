a8d1900d12161123c9a7050313bcc133a608f560
Mike Oram <mike.oram@mayden.co.uk>
2020-08-12 18:02:06 +0100

    RECORDED 02_11 SUCCESSFULLY

--- js/fetch.js
+++ js/fetch.js
@@ -3,3 +3,25 @@ async function fetchData(url) {
     const data = await response.json()
     return data
 }
+
+async function APIFetchData(url) {
+    let data
+    const response = await fetch(url)
+    if (response.ok) {
+        data = await response.json()
+        sessionStorage.setItem(url, JSON.stringify(data))
+    } else {
+        data = JSON.parse(sessionStorage.getItem(url))
+    }
+    return data
+}
+
+async function localFetchData(url) {
+    let data = JSON.parse(sessionStorage.getItem(url))
+    if (data === null) {
+        const response = await fetch(url)
+        data = await response.json()
+        sessionStorage.setItem(url, JSON.stringify(data))
+    }
+    return data
+}
